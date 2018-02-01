72d782af39d919f73c5ca8e831bc37ce8439213a
Anton Kuryan <a.kuryan@colours.nl>
2018-02-01 13:11:47 +0300

    Moved classes

--- src/NewRelic/Synthetics.Api/Classes/Monitor.cs
+++ src/NewRelic/Synthetics.Api/Classes/Monitor.cs
@@ -8,30 +8,6 @@ namespace NewRelic.Synthetics.Api.Classes
 
     using Newtonsoft.Json;
 
-    public class MonitorBase
-    {
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("type")]
-        public string Type { get; set; }
-
-        [JsonProperty("frequency")]
-        public int Frequency { get; set; }
-
-        [JsonProperty("uri")]
-        public string Uri { get; set; }
-
-        [JsonProperty("locations")]
-        public string[] Locations { get; set; }
-
-        [JsonProperty("status")]
-        public string Status { get; set; }
-
-        [JsonProperty("slaThreshold")]
-        public double SlaThreshold { get; set; }
-    }
-
     public class Monitor : MonitorBase
     {
         [JsonProperty("id")]
